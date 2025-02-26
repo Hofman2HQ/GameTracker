d6fcde79a199ab09cd1f824a43ed074bd19add50
Eliad Hofman <eliad633@walla.com>
2025-02-27 01:08:14 +0200

    Second commit

---
+++ MyGameCatalog/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyGameCatalog;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MyGameCatalog/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyGameCatalog;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
